0610122001686f349158043c7712bc3d1b6ffb33
goddardduncan <64201397+goddardduncan@users.noreply.github.com>
2025-03-18 16:33:46 +1100

    d
    d

--- m8/M8SliceGlitch/build/cmake_install.cmake
+++ m8/M8SliceGlitch/build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/ark/git/m8/SampleBrowser
+# Install script for directory: /home/ark/git/m8/M8SliceGlitch
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/ark/git/m8/SampleBrowser/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/ark/git/m8/M8SliceGlitch/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
